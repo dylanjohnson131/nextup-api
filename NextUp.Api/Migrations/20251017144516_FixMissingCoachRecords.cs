4872e66cfcad6105f6081ba6a80e0d161db6d4b9
Dylan Johnson <dylanjohnson263@gmail.com>
2025-10-17 09:53:37 -0500

    feat: Add migration to create missing Coach records for Lisa Martinez and Mark Stevens

---
+++ NextUp.Api/Migrations/20251017144516_FixMissingCoachRecords.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NextUp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMissingCoachRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Create Coach records for Lisa Martinez and Mark Stevens
+            migrationBuilder.Sql(@"
+                INSERT INTO ""Coach"" (""UserId"", ""TeamId"", ""ExperienceYears"", ""Specialty"", ""Certification"", ""Bio"", ""CreatedAt"", ""UpdatedAt"")
+                SELECT 
+                    u.""UserId"",
+                    t.""TeamId"",
+                    CASE 
+                        WHEN u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez' THEN 10
+                        WHEN u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens' THEN 6
+                    END as ""ExperienceYears"",
+                    CASE 
+                        WHEN u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez' THEN 'Team Coordination'
+                        WHEN u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens' THEN 'Speed & Agility'
+                    END as ""Specialty"",
+                    CASE 
+                        WHEN u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez' THEN 'Advanced Football Coach'
+                        WHEN u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens' THEN 'Level 2 Football Coach'
+                    END as ""Certification"",
+                    CASE 
+                        WHEN u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez' THEN 'Strategic coach with expertise in team building and coordination.'
+                        WHEN u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens' THEN 'Dynamic coach focusing on fast-paced offensive strategies.'
+                    END as ""Bio"",
+                    NOW(),
+                    NOW()
+                FROM ""Users"" u
+                JOIN ""Team"" t ON t.""CoachId"" = u.""UserId""
+                WHERE (u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez')
+                   OR (u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens')
+                AND NOT EXISTS (
+                    SELECT 1 FROM ""Coach"" c WHERE c.""UserId"" = u.""UserId""
+                );
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove the Coach records we created for Lisa Martinez and Mark Stevens
+            migrationBuilder.Sql(@"
+                DELETE FROM ""Coach"" 
+                WHERE ""UserId"" IN (
+                    SELECT u.""UserId"" 
+                    FROM ""Users"" u 
+                    WHERE (u.""FirstName"" = 'Lisa' AND u.""LastName"" = 'Martinez')
+                       OR (u.""FirstName"" = 'Mark' AND u.""LastName"" = 'Stevens')
+                );
+            ");
+        }
+    }
+}
